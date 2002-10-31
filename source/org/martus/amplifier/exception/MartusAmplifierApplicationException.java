f341d78a51a598862b583e0aff4a15dbf5021360
dan <dan@localhost>
2002-10-31 19:47:41 +0000

    Removing generated comments

--- source/org/martus/amplifier/exception/MartusAmplifierApplicationException.java
+++ source/org/martus/amplifier/exception/MartusAmplifierApplicationException.java
@@ -1,12 +1,24 @@
 package org.martus.amplifier.exception;
 
-/**
- * @author dchu
- *
- * To change this generated comment edit the template variable "typecomment":
- * Window>Preferences>Java>Templates.
- * To enable and disable the creation of type comments go to
- * Window>Preferences>Java>Code Generation.
- */
 public class MartusAmplifierApplicationException extends Exception
-{}
+{
+	public MartusAmplifierApplicationException()
+	{
+		super();
+	}
+
+	public MartusAmplifierApplicationException(String arg0)
+	{
+		super(arg0);
+	}
+
+	public MartusAmplifierApplicationException(String arg0, Throwable arg1)
+	{
+		super(arg0, arg1);
+	}
+
+	public MartusAmplifierApplicationException(Throwable arg0)
+	{
+		super(arg0);
+	}
+}
