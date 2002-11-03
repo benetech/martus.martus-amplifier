8e1c9c68224701516ece75ae2c41c1b3872bbf83
uid518 <uid518@localhost>
2002-11-03 03:08:33 +0000

    TestSuite for all internationalization infrastructure code

---
+++ source/org/martus/amplifier/test/i18n/TestAllI18N.java
@@ -0,0 +1,35 @@
+package org.martus.amplifier.test.i18n;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * @author Daniel Chu
+ *
+ */
+public class TestAllI18N extends TestSuite {
+	public TestAllI18N()
+	{
+		super();
+	}
+
+	public static void main (String[] args) 
+	{
+		runTests();
+	}
+
+	public static void runTests () 
+	{
+		junit.textui.TestRunner.run(suite());
+	}
+
+	public static Test suite ( ) 
+	{
+		TestSuite suite= new TestSuite("All Martus Amplifier i18n Tests");
+		
+		suite.addTest(new TestSuite(I18NManagerTest.class));
+	
+	    return suite;
+	}
+
+}
