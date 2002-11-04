f19d548da0e493c0b3a1d447f65be7311c88c133
uid518 <uid518@localhost>
2002-11-04 02:17:59 +0000

    added methods for accessing searching functionality

--- source/org/martus/amplifier/presentation/search/SearchResultsBean.java
+++ source/org/martus/amplifier/presentation/search/SearchResultsBean.java
@@ -1,11 +1,24 @@
 package org.martus.amplifier.presentation.search;
 
+import org.apache.lucene.search.Hits;
+import org.martus.amplifier.service.search.BulletinSearcher;
+
 public class SearchResultsBean
 {
 
 	public SearchResultsBean()
 	{
 		super();
+	}
+
+	public Hits getSearchResults(String field, String query)
+	{
+		return BulletinSearcher.getInstance().searchField(field, query);
+	}
+	
+	public Hits getSearchResults(String query)
+	{
+		return BulletinSearcher.getInstance().searchText(query);
 	}
 
 }
