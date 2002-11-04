d0573789f88a2ee6712bb1bd1e6c8551851afadc
uid518 <uid518@localhost>
2002-11-04 02:17:37 +0000

    Bean will be used for generating the search query page

---
+++ source/org/martus/amplifier/presentation/search/SearchQueryBean.java
@@ -0,0 +1,19 @@
+package org.martus.amplifier.presentation.search;
+
+import org.apache.lucene.search.Hits;
+import org.martus.amplifier.service.search.BulletinSearcher;
+
+public class SearchQueryBean
+{
+
+	public SearchQueryBean()
+	{
+		super();
+	}
+
+	public Hits getSearchFields()
+	{
+		return null;
+	}
+	
+}
