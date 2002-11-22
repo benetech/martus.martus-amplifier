d3908e1c5315982aa86033ab1f51d72c2776ca03
dan <dan@localhost>
2002-11-22 01:37:07 +0000

    starting class that directs the whole amplifier server update process

---
+++ source/org/martus/amplifier/service/datasynch/DataSynchManager.java
@@ -0,0 +1,31 @@
+package org.martus.amplifier.service.datasynch;
+
+import java.util.List;
+
+public class DataSynchManager
+{
+
+	public DataSynchManager()
+	{
+		super();
+	}
+	
+	private List getNewBulletinIds()
+	{
+		return null;
+	}
+	
+	private List getNewBulletinObjects()
+	{
+		return null;
+	}
+	
+	private boolean indexAndSaveNewBulletins()
+	{
+		return false;
+	}
+	
+	public void updateAmplifierBulletins()
+	{}	
+
+}
