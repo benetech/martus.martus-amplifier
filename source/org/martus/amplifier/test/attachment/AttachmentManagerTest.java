76c4ab2c5c3ea72f61091e76b738a2ea78e60729
uid518 <uid518@localhost>
2002-12-19 22:00:34 +0000

    Added tests for attachment extraction

--- source/org/martus/amplifier/test/attachment/AttachmentManagerTest.java
+++ source/org/martus/amplifier/test/attachment/AttachmentManagerTest.java
@@ -3,12 +3,35 @@ package org.martus.amplifier.test.attachment;
 import java.io.File;
 import java.util.List;
 
-import org.martus.common.UniversalId;
 import org.martus.amplifier.common.configuration.AmplifierConfiguration;
 import org.martus.amplifier.service.attachment.AttachmentManager;
+import org.martus.common.Base64;
+import org.martus.common.UniversalId;
+import org.martus.common.Base64.InvalidBase64Exception;
 
 public class AttachmentManagerTest extends AbstractAttachmentTest
 {
+	public void testExtractAttachment()
+	{		
+		AttachmentManager manager = AttachmentManager.getInstance();
+		String attachmentId = "A-40984b-eeeb80ef32--7fa6";
+		String testAttachmentXmlFilePath = 
+			AmplifierConfiguration.getInstance().getBasePath() + "\\testdata\\" + attachmentId;
+		File xmlFile = new File(testAttachmentXmlFilePath);
+		String sessionKey = "++WSe6fWUsf3UsQHr6nrr/cxxi0wa3s8LZPqP40ZwhE=";
+		byte[] decryptedSessionKey = null;
+		try
+		{
+			decryptedSessionKey = Base64.decode(sessionKey);
+		}
+		catch(InvalidBase64Exception ibe)
+		{}
+		String filePath = 
+			AmplifierConfiguration.getInstance().buildAmplifierWorkingPath("attachment_testing", attachmentId);
+		File destinationFile = new File(filePath);
+		manager.extractAttachment(xmlFile, decryptedSessionKey, destinationFile);	
+}
+
 	public void testPutAndGetId()
 	{		
 		AttachmentManager manager = AttachmentManager.getInstance();
@@ -25,9 +48,9 @@ public class AttachmentManagerTest extends AbstractAttachmentTest
 		AttachmentManager manager = AttachmentManager.getInstance();
 		UniversalId id = UniversalId.createDummyUniversalId();
 		File testDoc = 
-			new File(AmplifierConfiguration.getInstance().buildAmplifierWorkingPath(ATTACHMENT_TEST_FOLDER, "test.doc"));
+			new File(AmplifierConfiguration.getInstance().buildAmplifierBasePath(ATTACHMENT_TEST_INPUT_FOLDER, "test.doc"));
 		manager.putAttachmentFile(id, testDoc);
-		File returnDoc = manager.getAttachmentFile(id, AmplifierConfiguration.getInstance().buildAmplifierWorkingPath(ATTACHMENT_TEST_FOLDER, "testoutput.doc"));
+		File returnDoc = manager.getAttachmentFile(id, AmplifierConfiguration.getInstance().buildAmplifierWorkingPath(ATTACHMENT_TEST_OUTPUT_FOLDER), "testoutput.doc");
 		assertNotNull(returnDoc);
 	}
 	
@@ -40,5 +63,6 @@ public class AttachmentManagerTest extends AbstractAttachmentTest
 		assertNotNull(result);
 	}
 	
-	private static final String ATTACHMENT_TEST_FOLDER = "attachment_test_output";
+	private static final String ATTACHMENT_TEST_OUTPUT_FOLDER = "attachment_test_output";
+	private static final String ATTACHMENT_TEST_INPUT_FOLDER = "testdata";
 }
