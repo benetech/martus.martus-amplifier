47364d5160362c1ae0bd22340d3b49e5c78a20e6
dan <dan@localhost>
2003-01-09 23:47:58 +0000

    adding helper methods

--- source/org/martus/amplifier/presentation/attachment/AttachmentBean.java
+++ source/org/martus/amplifier/presentation/attachment/AttachmentBean.java
@@ -1,11 +1,41 @@
 package org.martus.amplifier.presentation.attachment;
 
-public class AttachmentBean
+import org.apache.lucene.document.Document;
+import org.martus.amplifier.service.attachment.AttachmentManager;
+import org.martus.amplifier.service.search.IBulletinConstants;
+import org.martus.common.UniversalId;
+import org.martus.common.UniversalId.NotUniversalIdException;
+
+public class AttachmentBean implements IBulletinConstants
 {
 
 	public AttachmentBean()
 	{
 		super();
 	}
-
+	
+	public void setDocument(Document newDoc)
+	{
+		System.out.println("hi");
+		doc = newDoc;
+	}
+	
+	public boolean bulletinHasAttachments()
+	{
+		boolean bulletinHasAttachments = true;
+		try
+		{
+			String universalIdString = doc.get(UNIVERSAL_ID_FIELD);
+			UniversalId universalId = UniversalId.createFromString(universalIdString);
+			bulletinHasAttachments = 
+				AttachmentManager.getInstance().hasAttachments(universalId);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Not universal id");
+		}
+		return bulletinHasAttachments;
+	}
+	
+	private Document doc = null;
 }
