23522fa9fa7dfa1ee8550ce7d630379516f7114e
uid505 <uid505@localhost>
2003-09-13 00:09:29 +0000

    Refactor

--- source/org/martus/amplifier/presentation/SimpleSearch.java
+++ source/org/martus/amplifier/presentation/SimpleSearch.java
@@ -1,57 +1,3 @@
-/*
- * The Apache Software License, Version 1.1
- *
- * Copyright (c) 2000-2001 The Apache Software Foundation.  All rights
- * reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution, if
- *    any, must include the following acknowlegement:
- *       "This product includes software developed by the
- *        Apache Software Foundation (http://www.apache.org/)."
- *    Alternately, this acknowlegement may appear in the software itself,
- *    if and wherever such third-party acknowlegements normally appear.
- *
- * 4. The names "The Jakarta Project", "Velocity", and "Apache Software
- *    Foundation" must not be used to endorse or promote products derived
- *    from this software without prior written permission. For written
- *    permission, please contact apache@apache.org.
- *
- * 5. Products derived from this software may not be called "Apache"
- *    nor may "Apache" appear in their names without prior written
- *    permission of the Apache Group.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the Apache Software Foundation.  For more
- * information on the Apache Software Foundation, please see
- * <http://www.apache.org/>.
- */
-
 package org.martus.amplifier.presentation;
 
 import java.io.IOException;
@@ -72,18 +18,8 @@ import org.apache.velocity.app.Velocity;
 import org.apache.velocity.exception.ResourceNotFoundException;
 import org.apache.velocity.exception.ParseErrorException;
 
-/**
- * Sample of how to use the VelocityServlet.
- * This example shows how to add objects to the context and
- * pass them to the template.
- * 
- * @author Dave Bryson
- * @author <a href="mailto:geirm@optonline.net">Geir Magnusson Jr.</a>
- * @version $Id$
- */
 public class SimpleSearch extends VelocityServlet
 {
-    
     /**
      *   Called by the VelocityServlet
      *   init().  We want to set a set of properties
@@ -127,32 +63,16 @@ public class SimpleSearch extends VelocityServlet
         return p;
     }
 
-
-    /**
-     *  <p>
-     *  main routine to handle a request.  Called by
-     *  VelocityServlet, your responsibility as programmer
-     *  is to simply return a valid Template
-     *  </p>
-     *
-     *  @param ctx a Velocity Context object to be filled with
-     *             data.  Will be used for rendering this 
-     *             template
-     *  @return Template to be used for request
-     */   
-    public Template handleRequest( HttpServletRequest request,
-	HttpServletResponse response, Context ctx )
-    {      
-    	
+    public Template handleRequest(HttpServletRequest request,
+								HttpServletResponse response, 
+								Context context)
+    {
     	HttpSession session = request.getSession();
     	String sessionId = (String)session.getAttribute("name");
     	if(sessionId == null)
     		sessionId = "my session";
     		
-    	ctx.put("session", sessionId);
-        /*
-         *  set up some data to put into the context
-         */
+    	context.put("session", sessionId);
 
         Vector fields = new Vector();
 		fields.addElement("Author");
@@ -163,37 +83,38 @@ public class SimpleSearch extends VelocityServlet
 		fields.addElement("Summary");
 		fields.addElement("Location");
 		fields.addElement("Entry Date");
-		ctx.put("theFields", fields);
+		context.put("theFields", fields);
 		
-		ctx.put("name", request.getParameter("query"));
+		context.put("name", request.getParameter("query"));
 		
-        /*
-         *  get the template.  There are three possible
-         *  exceptions.  Good to know what happened.
-         */
-
-        Template outty = null;
-        
-        try
-        {
-            outty =  getTemplate("SimpleSearch.vm");
-        }
-        catch( ParseErrorException pee )
-        {
-            System.out.println("SimpleSearch : parse error for template " + pee);
-        }
-        catch( ResourceNotFoundException rnfe )
-        {
-            System.out.println("SimpleSearch : template not found " + rnfe);
-        }
-        catch( Exception e )
-        {
-            System.out.println("Error " + e);
-        }
-        return outty;
+		return loadTemplate("SimpleSearch.vm");
     }
+
+
+	private Template loadTemplate(String templateName)
+	{
+		try
+		{
+			return getTemplate(templateName);
+		}
+		catch( ParseErrorException e )
+		{
+			displayError("parse error for template", e);
+		}
+		catch( ResourceNotFoundException e )
+		{
+			displayError("template not found", e);
+		}
+		catch( Exception e )
+		{
+			displayError("Unknown error", e);
+		}
+		return null;
+	}
+
+
+	private void displayError(String message, Exception e)
+	{
+		System.out.println(getClass().getName() + ": " + message + " " + e);
+	}
 }
-
-
-
-
