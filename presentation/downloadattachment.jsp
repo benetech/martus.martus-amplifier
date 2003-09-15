4c958a4afa34c843394b6acfac4b6717da89053e
uid505 <uid505@localhost>
2003-09-15 19:45:53 +0000

    Remove unused Tomcat, Berkeley DB, JSP stuff

--- presentation/downloadattachment.jsp
+++
@@ -1,2 +0,0 @@
-<%@ taglib prefix="amp" uri="amplifiertaglib" %>
-<amp:attachment accountId="${param.accountId}" localId="${param.localId}"/>
--- source/org/martus/amplifier/presentation/search/DownloadAttachmentTag.java
+++ source/org/martus/amplifier/presentation/search/DownloadAttachmentTag.java
@@ -7,12 +7,10 @@ import java.io.OutputStream;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.TagSupport;
 
-import org.apache.taglibs.standard.lang.support.ExpressionEvaluatorManager;
 import org.martus.amplifier.common.configuration.AmplifierConfiguration;
 import org.martus.amplifier.service.attachment.AttachmentManager;
 import org.martus.amplifier.service.attachment.AttachmentStorageException;
 import org.martus.amplifier.service.attachment.filesystem.FileSystemAttachmentManager;
-import org.martus.common.packet.UniversalId;
 import org.martus.util.StreamCopier;
 
 public class DownloadAttachmentTag extends TagSupport
@@ -36,19 +34,20 @@ public class DownloadAttachmentTag extends TagSupport
 
 	public int doEndTag() throws JspException 
 	{
-		String accountId = (String) ExpressionEvaluatorManager.evaluate(
-			"accountId", accountIdEL, String.class, this, pageContext);
-		String localId = (String) ExpressionEvaluatorManager.evaluate(
-			"localId", localIdEL, String.class, this, pageContext);
+// This is JSP-specific
+//		String accountId = (String) ExpressionEvaluatorManager.evaluate(
+//			"accountId", accountIdEL, String.class, this, pageContext);
+//		String localId = (String) ExpressionEvaluatorManager.evaluate(
+//			"localId", localIdEL, String.class, this, pageContext);
 			
 		AttachmentManager attachmentManager = null;
 		InputStream in = null;
 		
 		try {		
 			attachmentManager = openAttachmentManager();
-			UniversalId attachmentId = 
-				UniversalId.createFromAccountAndLocalId(accountId, localId);
-			in = attachmentManager.getAttachment(attachmentId);
+//			UniversalId attachmentId = 
+//				UniversalId.createFromAccountAndLocalId(accountId, localId);
+//			in = attachmentManager.getAttachment(attachmentId);
 			OutputStream out = pageContext.getResponse().getOutputStream();
 			new StreamCopier().copyStream(in, out);
 		} catch (AttachmentStorageException e) {
