20c3fc9d4ebf11a0d8ca9f63a8b49d7395714353
uid505 <uid505@localhost>
2003-09-17 15:07:19 +0000

    Refactor, mostly removing unneeded classes

--- source/org/martus/amplifier/main/MartusAmplifier.java
+++ source/org/martus/amplifier/main/MartusAmplifier.java
@@ -1,10 +1,20 @@
 package org.martus.amplifier.main;
 
 import java.io.File;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.logging.Logger;
 
 import org.martus.amplifier.common.configuration.AmplifierConfiguration;
+import org.martus.amplifier.service.attachment.AttachmentManager;
+import org.martus.amplifier.service.attachment.AttachmentStorageException;
+import org.martus.amplifier.service.attachment.filesystem.FileSystemAttachmentManager;
+import org.martus.amplifier.service.datasynch.BackupServerManager;
+import org.martus.amplifier.service.datasynch.DataSynchManager;
+import org.martus.amplifier.service.search.BulletinIndexException;
+import org.martus.amplifier.service.search.BulletinIndexer;
+import org.martus.amplifier.service.search.lucene.LuceneBulletinIndexer;
 
 public class MartusAmplifier
 {
@@ -15,12 +25,6 @@ public class MartusAmplifier
 		while(! isShutdownRequested() )
 		{
 		}
-	}
-	
-	static void execute()
-	{
-		MartusAmplifierDataSynch amplifierDataSynch = new MartusAmplifierDataSynch();
-		amplifierDataSynch.execute();
 	}
 	
 	static boolean isShutdownRequested()
@@ -50,6 +54,52 @@ public class MartusAmplifier
 	{
 		isSyncing = false;
 	}
+
+	static public void pullNewBulletinsFromServers(List backupServersList) 
+	{
+		Logger logger = Logger.getLogger("MainTask");
+				
+		BulletinIndexer indexer = null;
+		AttachmentManager attachmentManager = null;
+		try
+		{
+			DataSynchManager dataManager = new DataSynchManager(backupServersList);
+			AmplifierConfiguration config = 
+				AmplifierConfiguration.getInstance();
+			indexer = new LuceneBulletinIndexer(
+				config.getBasePath());
+			attachmentManager = new FileSystemAttachmentManager(
+				config.getBasePath());
+	
+			dataManager.getAllNewBulletins(attachmentManager, indexer);
+		}
+		catch(Exception e)
+		{
+			logger.severe("MartusAmplifierDataSynch.execute(): " + e.getMessage());
+			e.printStackTrace();
+		} 
+		finally
+		{
+			if (indexer != null) {
+				try {
+					indexer.close();
+				} catch (BulletinIndexException e) {
+					logger.severe(
+						"Unable to close the indexer: " + e.getMessage());
+				}
+			}
+			
+			if (attachmentManager != null) {
+				try {
+					attachmentManager.close();
+				} catch (AttachmentStorageException e) {
+					logger.severe(
+						"Unable to close the attachment manager: " +
+						e.getMessage());
+				}
+			}
+		}
+	}
 	
 	static class UpdateFromServerTask extends TimerTask
 	{	
@@ -60,7 +110,8 @@ public class MartusAmplifier
 				startSynch();
 				System.out.println("Scheduled Task started " + System.currentTimeMillis());
 
-				execute();
+				List backupServersList = new BackupServerManager().getBackupServersList();
+				MartusAmplifier.pullNewBulletinsFromServers(backupServersList);
 				
 				System.out.println("Scheduled Task finished " + System.currentTimeMillis() + "\n");
 				endSynch();
