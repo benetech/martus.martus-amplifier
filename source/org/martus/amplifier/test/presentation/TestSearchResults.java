1bdfce2e176f8aa01fdebafa2a3464176e43dacd
uid506 <uid506@localhost>
2003-09-19 19:09:29 +0000

    updated presentation to use common copyright added prev/next common vm page add prev/next to top of page

--- source/org/martus/amplifier/test/presentation/TestSearchResults.java
+++ source/org/martus/amplifier/test/presentation/TestSearchResults.java
@@ -38,21 +38,6 @@ public class TestSearchResults extends TestCaseEnhanced
 		HttpServletResponse response = null;
 		Context context = new MockContext();
 
-		final UniversalId uid1 = UniversalId.createDummyUniversalId();
-		final UniversalId uid2 = UniversalId.createDummyUniversalId();
-		class SearchResultsForTesting extends SearchResults
-		{
-			public List getSearchResults(AmplifierServletRequest request)
-				throws Exception, BulletinIndexException
-			{
-				if(request.getParameter("query")==null)
-					throw new Exception("malformed query");
-				Vector infos = new Vector();
-				infos.add(new BulletinInfo(uid1));
-				infos.add(new BulletinInfo(uid2));
-				return infos;
-			}
-		}
 		SearchResultsForTesting sr = new SearchResultsForTesting();
 		request.putParameter("query", "owiefijweofiejoifoiwjefoiwef");
 		String templateName = sr.selectTemplate(request, response, context);
@@ -69,4 +54,46 @@ public class TestSearchResults extends TestCaseEnhanced
 		assertEquals("NoSearchResults.vm", templateName);
 
 	}
+	
+/*	private Context createSampleSearchResults(MockAmplifierRequest request, HttpServletResponse response) throws Exception
+	{
+		Context context = new MockContext();
+		SearchResultsForTesting sr = new SearchResultsForTesting();
+		request.putParameter("query", "test");
+		request.parameters.put("index","1");
+		sr.selectTemplate(request, response, context);
+		return context;
+	}
+*/
+	final UniversalId uid1 = UniversalId.createDummyUniversalId();
+	final UniversalId uid2 = UniversalId.createDummyUniversalId();
+	final UniversalId uid3 = UniversalId.createDummyUniversalId();
+	final String bulletin1Title = "title 1";
+	final String bulletin2Title = "title 2";
+	final String bulletin3Title = "title 3";
+
+
+	class SearchResultsForTesting extends SearchResults
+	{
+		public List getSearchResults(AmplifierServletRequest request)
+			throws Exception, BulletinIndexException
+		{
+			if(request.getParameter("query")==null)
+				throw new Exception("malformed query");
+			Vector infos = new Vector();
+			BulletinInfo bulletinInfo1 = new BulletinInfo(uid1);
+			bulletinInfo1.set("title", bulletin1Title);
+			infos.add(bulletinInfo1);
+			
+			BulletinInfo bulletinInfo2 = new BulletinInfo(uid2);
+			bulletinInfo2.set("title", bulletin2Title);
+			infos.add(bulletinInfo2);
+			
+			BulletinInfo bulletinInfo3 = new BulletinInfo(uid3);
+			bulletinInfo3.set("title", bulletin3Title);
+			infos.add(bulletinInfo3);
+			return infos;
+		}
+	}
+	
 }
